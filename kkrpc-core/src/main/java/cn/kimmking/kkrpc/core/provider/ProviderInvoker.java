5953be946358450b997cf4cc7e2582ea8dad7a21
kimmking <kimmking@163.com>
2024-03-20 20:25:02 +0800

    refactor provider invoker

---
+++ kkrpc-core/src/main/java/cn/kimmking/kkrpc/core/provider/ProviderInvoker.java
@@ -0,0 +1,62 @@
+package cn.kimmking.kkrpc.core.provider;
+
+import cn.kimmking.kkrpc.core.api.RpcRequest;
+import cn.kimmking.kkrpc.core.api.RpcResponse;
+import cn.kimmking.kkrpc.core.meta.ProviderMeta;
+import cn.kimmking.kkrpc.core.util.TypeUtils;
+import org.springframework.util.MultiValueMap;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Description for this class.
+ *
+ * @Author : kimmking(kimmking@apache.org)
+ * @create 2024/3/20 20:16
+ */
+public class ProviderInvoker {
+
+    private MultiValueMap<String, ProviderMeta> skeleton;
+
+    public ProviderInvoker(ProviderBootstrap providerBootstrap) {
+        this.skeleton = providerBootstrap.getSkeleton();
+    }
+
+    public RpcResponse<Object> invoke(RpcRequest request) {
+        RpcResponse<Object> rpcResponse = new RpcResponse<>();
+        List<ProviderMeta> providerMetas = skeleton.get(request.getService());
+        try {
+            ProviderMeta meta = findProviderMeta(providerMetas, request.getMethodSign());
+            Method method = meta.getMethod();
+            Object[] args = processArgs(request.getArgs(), method.getParameterTypes());
+            Object result = method.invoke(meta.getServiceImpl(), args);
+            rpcResponse.setStatus(true);
+            rpcResponse.setData(result);
+            return rpcResponse;
+        } catch (InvocationTargetException e) {
+            rpcResponse.setEx(new RuntimeException(e.getTargetException().getMessage()));
+        } catch (IllegalAccessException e) {
+            rpcResponse.setEx(new RuntimeException(e.getMessage()));
+        }
+        return rpcResponse;
+    }
+
+    private Object[] processArgs(Object[] args, Class<?>[] parameterTypes) {
+        if(args == null || args.length == 0) return args;
+        Object[] actuals = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            actuals[i] = TypeUtils.cast(args[i], parameterTypes[i]);
+        }
+        return actuals;
+    }
+
+    private ProviderMeta findProviderMeta(List<ProviderMeta> providerMetas, String methodSign) {
+        Optional<ProviderMeta> optional = providerMetas.stream()
+                .filter(x -> x.getMethodSign().equals(methodSign)).findFirst();
+        return optional.orElse(null);
+    }
+
+}
--- kkrpc-demo-provider/src/main/java/cn/kimmking/kkrpc/demo/provider/KkrpcDemoProviderApplication.java
+++ kkrpc-demo-provider/src/main/java/cn/kimmking/kkrpc/demo/provider/KkrpcDemoProviderApplication.java
@@ -4,6 +4,7 @@ import cn.kimmking.kkrpc.core.api.RpcRequest;
 import cn.kimmking.kkrpc.core.api.RpcResponse;
 import cn.kimmking.kkrpc.core.provider.ProviderBootstrap;
 import cn.kimmking.kkrpc.core.provider.ProviderConfig;
+import cn.kimmking.kkrpc.core.provider.ProviderInvoker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -26,34 +27,34 @@ public class KkrpcDemoProviderApplication {
     // 使用HTTP + JSON 来实现序列化和通信
 
     @Autowired
-    ProviderBootstrap providerBootstrap;
+    ProviderInvoker providerInvoker;
 
     @RequestMapping("/")
-    public RpcResponse invoke(@RequestBody RpcRequest request) {
-        return providerBootstrap.invoke(request);
+    public RpcResponse<Object> invoke(@RequestBody RpcRequest request) {
+        return providerInvoker.invoke(request);
     }
 
 
     @Bean
     ApplicationRunner providerRun() {
         return x -> {
-//            // test 1 parameter method
-//            RpcRequest request = new RpcRequest();
-//            request.setService("cn.kimmking.kkrpc.demo.api.UserService");
-//            request.setMethodSign("findById@1_int");
-//            request.setArgs(new Object[]{100});
-//
-//            RpcResponse rpcResponse = invoke(request);
-//            System.out.println("return : "+rpcResponse.getData());
-//
-//            // test 2 parameters method
-//            RpcRequest request1 = new RpcRequest();
-//            request1.setService("cn.kimmking.kkrpc.demo.api.UserService");
-//            request1.setMethodSign("findById@2_int_java.lang.String");
-//            request1.setArgs(new Object[]{100, "CC"});
-//
-//            RpcResponse rpcResponse1 = invoke(request1);
-//            System.out.println("return : "+rpcResponse1.getData());
+            // test 1 parameter method
+            RpcRequest request = new RpcRequest();
+            request.setService("cn.kimmking.kkrpc.demo.api.UserService");
+            request.setMethodSign("findById@1_int");
+            request.setArgs(new Object[]{100});
+
+            RpcResponse<Object> rpcResponse = invoke(request);
+            System.out.println("return : "+rpcResponse.getData());
+
+            // test 2 parameters method
+            RpcRequest request1 = new RpcRequest();
+            request1.setService("cn.kimmking.kkrpc.demo.api.UserService");
+            request1.setMethodSign("findById@2_int_java.lang.String");
+            request1.setArgs(new Object[]{100, "CC"});
+
+            RpcResponse<Object> rpcResponse1 = invoke(request1);
+            System.out.println("return : "+rpcResponse1.getData());
 
         };
     }

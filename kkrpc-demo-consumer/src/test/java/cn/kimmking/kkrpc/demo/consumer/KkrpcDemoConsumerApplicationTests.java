c69cbd28335b1215229c818e8cc4d88e226750c3
kimmking <kimmking@163.com>
2024-03-24 20:02:59 +0800

    update consumer application test case

--- kkrpc-demo-consumer/src/test/java/cn/kimmking/kkrpc/demo/consumer/KkrpcDemoConsumerApplicationTests.java
+++ kkrpc-demo-consumer/src/test/java/cn/kimmking/kkrpc/demo/consumer/KkrpcDemoConsumerApplicationTests.java
@@ -1,13 +1,31 @@
 package cn.kimmking.kkrpc.demo.consumer;
 
+import cn.kimmking.kkrpc.demo.provider.KkrpcDemoProviderApplication;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class KkrpcDemoConsumerApplicationTests {
 
+    static ApplicationContext context;
+    @BeforeAll
+    static void init() {
+        context = SpringApplication.run(KkrpcDemoProviderApplication.class,
+                "--server.port=8084", "--logging.level.cn.kimmking.kkrpc=debug");
+    }
+
     @Test
     void contextLoads() {
+        System.out.println(" ===> aaaa  .... ");
+    }
+
+    @AfterAll
+    static void destory() {
+        SpringApplication.exit(context, () -> 1);
     }
 
 }

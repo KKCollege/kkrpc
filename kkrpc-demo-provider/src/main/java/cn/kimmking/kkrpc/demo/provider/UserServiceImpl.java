26430249a7b5d2d456fc5f893b90ec00c96c5b23
kimmking <kimmking@163.com>
2024-03-31 21:15:58 +0800

    polish codes

--- kkrpc-demo-provider/src/main/java/cn/kimmking/kkrpc/demo/provider/UserServiceImpl.java
+++ kkrpc-demo-provider/src/main/java/cn/kimmking/kkrpc/demo/provider/UserServiceImpl.java
@@ -27,7 +27,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User findById(int id) {
-        return new User(id, "KK-"
+        return new User(id, "KK-V1-"
                 + environment.getProperty("server.port")
                 + "_" + System.currentTimeMillis());
     }

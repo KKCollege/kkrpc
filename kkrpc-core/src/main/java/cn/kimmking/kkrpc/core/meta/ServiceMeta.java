d1bc1be12ba0745945d9a3dfbe7ea328e95f45d6
kimmking <kimmking@163.com>
2024-03-31 21:41:50 +0800

    add service metas

--- kkrpc-core/src/main/java/cn/kimmking/kkrpc/core/meta/ServiceMeta.java
+++ kkrpc-core/src/main/java/cn/kimmking/kkrpc/core/meta/ServiceMeta.java
@@ -1,7 +1,11 @@
 package cn.kimmking.kkrpc.core.meta;
 
+import com.alibaba.fastjson.JSON;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 描述服务元数据.
@@ -18,9 +22,14 @@ public class ServiceMeta {
     private String namespace;
     private String env;
     private String name;
-    // private String version;
+
+    private Map<String, String> parameters = new HashMap<>();
 
     public String toPath() {
         return String.format("%s_%s_%s_%s", app, namespace, env, name);
+    }
+
+    public String toMetas() {
+        return JSON.toJSONString(this.getParameters());
     }
 }

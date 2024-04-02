ea71b58752e670d2786f71322b9612c9a0dc3504
kimmking <kimmking@163.com>
2024-04-03 07:37:33 +0800

    add enable kkrpc

---
+++ kkrpc-core/src/main/java/cn/kimmking/kkrpc/core/annotation/EnableKkrpc.java
@@ -0,0 +1,23 @@
+package cn.kimmking.kkrpc.core.annotation;
+
+import cn.kimmking.kkrpc.core.consumer.ConsumerConfig;
+import cn.kimmking.kkrpc.core.provider.ProviderConfig;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+/**
+ * 组合一个入口.
+ *
+ * @Author : kimmking(kimmking@apache.org)
+ * @create 2024/4/2 22:49
+ */
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Inherited
+@Import({ProviderConfig.class, ConsumerConfig.class})
+public @interface EnableKkrpc {
+
+}

1118af5d710dd6fe353c1e5862eb36de50305372
kimmking <kimmking@163.com>
2024-04-24 21:01:40 +0800

    implement subscribe method

--- kkrpc-core/src/main/java/cn/kimmking/kkrpc/core/registry/kk/KkRegistryCenter.java
+++ kkrpc-core/src/main/java/cn/kimmking/kkrpc/core/registry/kk/KkRegistryCenter.java
@@ -1,13 +1,22 @@
 package cn.kimmking.kkrpc.core.registry.kk;
 
 import cn.kimmking.kkrpc.core.api.RegistryCenter;
+import cn.kimmking.kkrpc.core.consumer.HttpInvoker;
 import cn.kimmking.kkrpc.core.meta.InstanceMeta;
 import cn.kimmking.kkrpc.core.meta.ServiceMeta;
 import cn.kimmking.kkrpc.core.registry.ChangedListener;
+import cn.kimmking.kkrpc.core.registry.Event;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * implementation for kk registry center.
@@ -24,31 +33,60 @@ public class KkRegistryCenter implements RegistryCenter {
     @Override
     public void start() {
         log.info(" ====>>>> [KKRegistry] : start with server : {}", servers);
+        executor = Executors.newScheduledThreadPool(1);
     }
 
     @Override
     public void stop() {
         log.info(" ====>>>> [KKRegistry] : stop with server : {}", servers);
+        executor.shutdown();
+        try {
+            executor.awaitTermination(1000, TimeUnit.MILLISECONDS);
+            if(!executor.isTerminated()) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            // ignore
+        }
     }
 
     @Override
     public void register(ServiceMeta service, InstanceMeta instance) {
-        log.info(" ====>>>> [KKRegistry] : registry instance {} for {}", instance, service);
-
+        log.info(" ====>>>> [KKRegistry] : register instance {} for {}", instance, service);
+        HttpInvoker.httpPost(JSON.toJSONString(instance), servers + "/reg?service=" + service.toPath(), InstanceMeta.class);
+        log.info(" ====>>>> [KKRegistry] : registered {}", instance);
     }
 
     @Override
     public void unregister(ServiceMeta service, InstanceMeta instance) {
-
+        log.info(" ====>>>> [KKRegistry] : unregister instance {} for {}", instance, service);
+        HttpInvoker.httpPost(JSON.toJSONString(instance), servers + "/unreg?service=" + service.toPath(), InstanceMeta.class);
+        log.info(" ====>>>> [KKRegistry] : unregistered {}", instance);
     }
 
     @Override
     public List<InstanceMeta> fetchAll(ServiceMeta service) {
-        return List.of();
+        log.info(" ====>>>> [KKRegistry] : find all instances for {}", service);
+        List<InstanceMeta> instances = HttpInvoker.httpGet(servers + "/findAll?service=" + service.toPath(), new TypeReference<List<InstanceMeta>>() {
+        });
+        log.info(" ====>>>> [KKRegistry] : findAll = {}", instances);
+        return instances;
     }
+
+    Map<String, Long> VERSIONS = new HashMap<>();
+    ScheduledExecutorService executor = null;
 
     @Override
     public void subscribe(ServiceMeta service, ChangedListener listener) {
-
+        executor.scheduleWithFixedDelay( () -> {
+            Long version = VERSIONS.getOrDefault(service.toPath(), -1L);
+            Long newVersion = HttpInvoker.httpGet(servers + "/version?service=" + service.toPath(), Long.class);
+            log.info(" ====>>>> [KKRegistry] : version = {}, newVersion = {}", version, newVersion);
+            if(newVersion > version) {
+                List<InstanceMeta> instances = fetchAll(service);
+                listener.fire(new Event(instances));
+                VERSIONS.put(service.toPath(), newVersion);
+            }
+        }, 1000, 5000, TimeUnit.MILLISECONDS);
     }
 }

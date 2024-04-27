ab7f506b237a7e0ea8870718b565974a96aa1683
kimmking <kimmking@163.com>
2024-04-27 19:39:35 +0800

    refactor KK registry center

--- kkrpc-core/src/main/java/cn/kimmking/kkrpc/core/registry/kk/KkRegistryCenter.java
+++ kkrpc-core/src/main/java/cn/kimmking/kkrpc/core/registry/kk/KkRegistryCenter.java
@@ -9,6 +9,7 @@ import cn.kimmking.kkrpc.core.registry.Event;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -29,58 +30,36 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class KkRegistryCenter implements RegistryCenter {
 
+    private static final String REG_PATH = "/reg";
+    private static final String UNREG_PATH = "/unreg";
+    private static final String FINDALL_PATH = "/findAll";
+    private static final String VERSION_PATH = "/version";
+    private static final String RENEWS_PATH = "/renews";
+
     @Value("${kkregistry.servers}")
     private String servers;
 
     Map<String, Long> VERSIONS = new HashMap<>();
     MultiValueMap<InstanceMeta, ServiceMeta> RENEWS = new LinkedMultiValueMap<>();
-    ScheduledExecutorService consumerExecutor = null;
-    ScheduledExecutorService producerExecutor = null;
+    KkHealthChecker healthChecker = new KkHealthChecker();
 
     @Override
     public void start() {
         log.info(" ====>>>> [KKRegistry] : start with server : {}", servers);
-        consumerExecutor = Executors.newScheduledThreadPool(1);
-        producerExecutor = Executors.newScheduledThreadPool(1);
-        producerExecutor.scheduleAtFixedRate(() -> {
-            RENEWS.keySet().stream().forEach(
-                    instance -> {
-                        StringBuffer sb = new StringBuffer();
-                        for (ServiceMeta service : RENEWS.get(instance)) {
-                            sb.append(service.toPath()).append(",");
-                        }
-                        String services = sb.toString();
-                        if(services.endsWith(",")) services = services.substring(0, services.length() - 1);
-                        Long timestamp = HttpInvoker.httpPost(JSON.toJSONString(instance), servers + "/renews?services=" + services, Long.class);
-                        log.info(" ====>>>> [KKRegistry] : renew instance {} for {} at {}", instance, services, timestamp);
-                    }
-            );
-        }, 5, 5, TimeUnit.SECONDS);
+        healthChecker.start();
+        providerCheck();
     }
 
     @Override
     public void stop() {
         log.info(" ====>>>> [KKRegistry] : stop with server : {}", servers);
-        gracefulShutdown(consumerExecutor);
-        gracefulShutdown(producerExecutor);
-    }
-
-    private void gracefulShutdown(ScheduledExecutorService executorService) {
-        executorService.shutdown();
-        try {
-            executorService.awaitTermination(1000, TimeUnit.MILLISECONDS);
-            if(!executorService.isTerminated()) {
-                executorService.shutdownNow();
-            }
-        } catch (InterruptedException e) {
-            // ignore
-        }
+        healthChecker.stop();
     }
 
     @Override
     public void register(ServiceMeta service, InstanceMeta instance) {
         log.info(" ====>>>> [KKRegistry] : register instance {} for {}", instance, service);
-        HttpInvoker.httpPost(JSON.toJSONString(instance), servers + "/reg?service=" + service.toPath(), InstanceMeta.class);
+        HttpInvoker.httpPost(JSON.toJSONString(instance), regPath(service), InstanceMeta.class);
         log.info(" ====>>>> [KKRegistry] : registered {}", instance);
         RENEWS.add(instance, service);
     }
@@ -88,7 +67,7 @@ public class KkRegistryCenter implements RegistryCenter {
     @Override
     public void unregister(ServiceMeta service, InstanceMeta instance) {
         log.info(" ====>>>> [KKRegistry] : unregister instance {} for {}", instance, service);
-        HttpInvoker.httpPost(JSON.toJSONString(instance), servers + "/unreg?service=" + service.toPath(), InstanceMeta.class);
+        HttpInvoker.httpPost(JSON.toJSONString(instance), unregPath(service), InstanceMeta.class);
         log.info(" ====>>>> [KKRegistry] : unregistered {}", instance);
         RENEWS.remove(instance, service);
     }
@@ -96,23 +75,67 @@ public class KkRegistryCenter implements RegistryCenter {
     @Override
     public List<InstanceMeta> fetchAll(ServiceMeta service) {
         log.info(" ====>>>> [KKRegistry] : find all instances for {}", service);
-        List<InstanceMeta> instances = HttpInvoker.httpGet(servers + "/findAll?service=" + service.toPath(), new TypeReference<List<InstanceMeta>>() {
+        List<InstanceMeta> instances = HttpInvoker.httpGet(findAllPath(service), new TypeReference<List<InstanceMeta>>() {
         });
         log.info(" ====>>>> [KKRegistry] : findAll = {}", instances);
         return instances;
     }
 
+    public void providerCheck() {
+        healthChecker.providerCheck(() -> {
+            RENEWS.keySet().stream().forEach(
+                    instance -> {
+                        Long timestamp = HttpInvoker.httpPost(JSON.toJSONString(instance),
+                                renewsPath(RENEWS.get(instance)), Long.class);
+                        log.info(" ====>>>> [KKRegistry] : renew instance {} at {}", instance, timestamp);
+                    }
+            );
+        });
+    }
+
     @Override
     public void subscribe(ServiceMeta service, ChangedListener listener) {
-        consumerExecutor.scheduleWithFixedDelay( () -> {
+        healthChecker.consumerCheck( () -> {
             Long version = VERSIONS.getOrDefault(service.toPath(), -1L);
-            Long newVersion = HttpInvoker.httpGet(servers + "/version?service=" + service.toPath(), Long.class);
+            Long newVersion = HttpInvoker.httpGet(versionPath(service), Long.class);
             log.info(" ====>>>> [KKRegistry] : version = {}, newVersion = {}", version, newVersion);
             if(newVersion > version) {
                 List<InstanceMeta> instances = fetchAll(service);
                 listener.fire(new Event(instances));
                 VERSIONS.put(service.toPath(), newVersion);
             }
-        }, 1000, 5000, TimeUnit.MILLISECONDS);
+        });
     }
+
+    private String regPath(ServiceMeta service) {
+        return path(REG_PATH, service);
+    }
+    private String unregPath(ServiceMeta service) {
+        return path(UNREG_PATH, service);
+    }
+    private String findAllPath(ServiceMeta service) {
+        return path(FINDALL_PATH, service);
+    }
+    private String versionPath(ServiceMeta service) {
+        return path(VERSION_PATH, service);
+    }
+    private String path(String context, ServiceMeta service) {
+        return servers + context + "?service=" + service.toPath();
+    }
+
+    private String renewsPath(List<ServiceMeta> serviceList) {
+        return path(RENEWS_PATH, serviceList);
+    }
+
+    private String path(String context, List<ServiceMeta> serviceList) {
+        StringBuffer sb = new StringBuffer();
+        for (ServiceMeta service : serviceList) {
+            sb.append(service.toPath()).append(",");
+        }
+        String services = sb.toString();
+        if(services.endsWith(",")) services = services.substring(0, services.length() - 1);
+        log.info(" ====>>>> [KKRegistry] : renew instance for {}", services);
+        return servers + context + "?services=" + services;
+    }
+
 }
